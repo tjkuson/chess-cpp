9d2e0028bdfe1e23003e4f4499fe89045f512d71
tomjk@tuta.iO <tomjk@tuta.io>
2021-05-18 19:58:23 +0100

    Added copyright notice

--- src/bishop.cpp
+++ src/bishop.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2021, Tom Kuson.
+// This file is a part of Chess CLI which is released under the GPLv3.
+// See LICENSE file in the project root or go to <https://www.gnu.org/licenses/> for full license details.
+
 // bishop.cpp
 
 #include "bishop.h"
