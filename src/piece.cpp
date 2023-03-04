5cc02c3eb8e792c364a119ec79bdaf22900da4cd
Tom Kuson <12143269+tjkuson@users.noreply.github.com>
2023-03-04 17:10:02 +0000

    add piece base class

--- src/piece.cpp
+++ src/piece.cpp
@@ -1,5 +1,23 @@
-//
-// Created by tjkuson on 04/03/23.
-//
-
 #include "piece.h"
+
+#include <utility>
+
+// Constructor; note that the position is passed by value and then moved into the position member variable
+Piece::Piece(std::pair<int, int> position, Colour colour) : position(std::move(position)), colour(colour) {}
+// Destructor
+Piece::~Piece() noexcept = default;
+
+// Getters and setters
+auto Piece::get_position() const -> std::pair<int, int>
+{
+    return position;
+}
+auto Piece::get_colour() const -> Colour
+{
+    return colour;
+}
+void Piece::set_position(std::pair<int, int> pos)
+{
+    Piece::position = pos;
+}
+// Piece::set_colour is not defined because the colour of a piece is immutable
--- src/piece.h
+++ src/piece.h
@@ -1,26 +1,22 @@
-// Chess CLI: command-line chess
-// Copyright (c) 2023 Tom Kuson
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-//
-// Created by tjkuson on 04/03/23.
-//
-
 #ifndef CHESS_CLI_PIECE_H
 #define CHESS_CLI_PIECE_H
 
-class piece {
+#include "colour.h"
+#include <utility>
+class Piece {
+protected:
+    const Colour colour;
+    std::pair<int, int> position;
+
+public:
+    // Prototype for the constructor
+    Piece(std::pair<int, int> position, Colour colour);
+    // Prototype for the destructor
+    virtual ~Piece() noexcept;
+    // Prototype getters and setters
+    [[nodiscard]] auto get_position() const -> std::pair<int, int>;
+    [[nodiscard]] auto get_colour() const -> Colour;
+    void set_position(std::pair<int, int> pos);
 };
 
 #endif//CHESS_CLI_PIECE_H
